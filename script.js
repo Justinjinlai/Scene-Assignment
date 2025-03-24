94daa6c6c7e75e9cb7f7a7291d653708182714a1
Justinjinlai <justinjinlai@gmail.com>
2025-03-23 22:59:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('sceneCanvas');
+const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "#87CEEB"; // Light blue sky
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const backgroundImage = new Image();
+backgroundImage.src = 'images/background.jpg';
+backgroundImage.onload = function() {
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+};
+
+const foregroundImage1 = new Image();
+foregroundImage1.src = 'images/foreground1.png';
+foregroundImage1.onload = function() {
+    ctx.drawImage(foregroundImage1, 100, 100, 150, 150);
+};
+
+const foregroundImage2 = new Image();
+foregroundImage2.src = 'images/foreground2.png';
+foregroundImage2.onload = function() {
+    ctx.drawImage(foregroundImage2, 400, 250, 200, 200);
+};
+
+ctx.font = '30px Arial';
+ctx.fillStyle = 'white';
+ctx.fillText('Your Name', 20, 40);
+ctx.fillText('Scene Title', 20, 80);
