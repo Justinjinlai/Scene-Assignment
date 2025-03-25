315d5900bf7fc26f46804983d613c6ff561c2665
Justinjinlai <justinjinlai@gmail.com>
2025-03-25 17:47:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const canvas = document.getElementById('sceneCanvas');
 const ctx = canvas.getContext('2d');
 
 const backgroundImage = new Image();
-backgroundImage.src = 'images/background.jpg';
+backgroundImage.src = 'City.jpg';
 backgroundImage.onload = function() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 };
 
 const foregroundImage1 = new Image();
-foregroundImage1.src = 'images/foreground1.png';
+foregroundImage1.src = 'M9.jpg';
 foregroundImage1.onload = function() {
     ctx.drawImage(foregroundImage1, 100, 100, 150, 150);
 };
 
 const foregroundImage2 = new Image();
-foregroundImage2.src = 'images/foreground2.png';
+foregroundImage2.src = 'M5.jpg';
 foregroundImage2.onload = function() {
     ctx.drawImage(foregroundImage2, 400, 250, 200, 200);
 };
