d8ae2f312a5067dcc4d7a950782e18b97feb7ae8
Justinjinlai <justinjinlai@gmail.com>
2025-03-25 19:31:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,39 @@ const canvas = document.getElementById('sceneCanvas');
 const ctx = canvas.getContext('2d');
 
 const backgroundImage = new Image();
-backgroundImage.src = 'City.jpg';
+backgroundImage.src = 'images/City.jpg';  
 backgroundImage.onload = function() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    drawForegroundImages();
 };
 
-const foregroundImage1 = new Image();
-foregroundImage1.src = 'M8.jpg';
-foregroundImage1.onload = function() {
-    ctx.drawImage(foregroundImage1, 100, 100, 150, 150);
-};
+function drawForegroundImages() {
+    const foregroundImage1 = new Image();
+    foregroundImage1.src = 'images/M8.jpg';  
+    foregroundImage1.onload = function() {
+        ctx.drawImage(foregroundImage1, 100, 350, 150, 150);
+    };
 
-const foregroundImage2 = new Image();
-foregroundImage2.src = 'M5.jpg';
-foregroundImage2.onload = function() {
-    ctx.drawImage(foregroundImage2, 400, 250, 200, 200);
-};
+    const foregroundImage2 = new Image();
+    foregroundImage2.src = 'images/M5.jpg';  
+    foregroundImage2.onload = function() {
+        ctx.drawImage(foregroundImage2, 500, 350, 200, 200);
+    };
 
-ctx.font = '30px Arial';
-ctx.fillStyle = 'white';
-ctx.fillText('Justin Jin Lai', 20, 40);
-ctx.fillText('City Scene', 20, 80);
+    drawText();  
+}
+
+function drawText() {
+    ctx.font = '35px Arial';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+
+    ctx.shadowColor = 'black';
+    ctx.shadowBlur = 4;
+    ctx.shadowOffsetX = 2;
+    ctx.shadowOffsetY = 2;
+
+    ctx.fillText('Justin Jin Lai', canvas.width / 2, 20);
+    ctx.fillText('City Scene', canvas.width / 2, 70);
+}
