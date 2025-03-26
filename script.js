7d6c9693a22e62e5246c5fd151115bccf877b036
Justinjinlai <justinjinlai@gmail.com>
2025-03-25 23:11:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,40 @@ const canvas = document.getElementById('sceneCanvas');
 const ctx = canvas.getContext('2d');
 
 const backgroundImage = new Image();
-backgroundImage.src = 'City.jpg';
-backgroundImage.onload = function() {
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-};
-
 const foregroundImage1 = new Image();
-foregroundImage1.src = 'M8.jpg';
-foregroundImage1.onload = function() {
-    ctx.drawImage(foregroundImage1, 100, 100, 150, 150);
-};
-
 const foregroundImage2 = new Image();
-foregroundImage2.src = 'M5.jpg';
-foregroundImage2.onload = function() {
-    ctx.drawImage(foregroundImage2, 400, 250, 200, 200);
-};
 
-ctx.font = '30px Arial';
-ctx.fillStyle = 'white';
-ctx.fillText('Justin Jin Lai', 20, 40);
-ctx.fillText('City Scene', 20, 80);
+backgroundImage.src = 'City.jpg';
+foregroundImage1.src = 'M5.jpg';
+foregroundImage2.src = 'M8.jpg';
+
+let imagesLoaded = 0;
+
+function checkAllLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === 3) {
+        drawScene();
+    }
+}
+
+backgroundImage.onload = checkAllLoaded;
+foregroundImage1.onload = checkAllLoaded;
+foregroundImage2.onload = checkAllLoaded;
+
+function drawScene() {
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(foregroundImage1, 50, 250, 300, 200);
+    ctx.drawImage(foregroundImage2, 350, 250, 400, 200);
+
+    drawText();
+}
+
+function drawText() {
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'black';
+    ctx.shadowColor = 'white';
+    ctx.shadowBlur = 4;
+    ctx.fillText('Justin Jin Lai', 20, 40);
+    ctx.fillText('City Scene', 20, 80);
+}
